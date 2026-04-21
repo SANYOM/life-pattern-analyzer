5c66b533928b3f6905e62df1d4103566b76ddff8
SANYOM THAREJA <sanyomthareja@gmail.com>
2026-04-22 00:54:45 +0530

    Update model.py

--- model.py
+++ model.py
@@ -2,19 +2,20 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 import joblib
 
-# Load dataset
 df = pd.read_csv("dataset.csv")
 
-# Clean column names (IMPORTANT)
+# Clean column names
 df.columns = df.columns.str.strip().str.lower().str.replace(" ", "_")
 
-# Features
+print("Columns:", df.columns)
+
+# Adjust these if needed
 X = df[["sleep_hours", "study_hours", "screen_time", "stress_level"]]
 y = df["productivity_score"]
 
-# Train model
 model = LinearRegression()
 model.fit(X, y)
 
-# Save model
 joblib.dump(model, "model.pkl")
+
+print("Model saved!")
